94ccc5b0dba381555e0b77895f2cd54dbd39c0b2
danfyfe <fyfedaniel@gmail.com>
2019-07-31 16:16:53 -0400

    header

--- src/App.js
+++ src/App.js
@@ -4,22 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="main">
-      <header>
+      <div className='header'>
         <div className='logo-container'>
           <img className='logo' src='https://i.imgur.com/z0gkiOz.png' alt='logo of dance party'/>
         </div>
         <div className='title-container'>
           <h1>Let's Do Something!</h1>
         </div>
-        
-        {/*<div className='menu-container'>
-          <div className='menu'>
-            <div className='menu-bar-1'></div>
-            <div className='menu-bar-2'></div>
-            <div className='menu-bar-3'></div>
-          </div>
-        </div>*/}
-      </header>
+
+      </div>
       <div className='content'>
       </div>
 
