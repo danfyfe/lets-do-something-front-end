a478d0acf6e7afcd907a3f02b1302189cd223d8f
danfyfe <fyfedaniel@gmail.com>
2019-08-04 11:44:03 -0400

    calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,6 +3,10 @@ import React from 'react'
 const Calendar = () => {
   return(
     <div className='calendar-container'>
+      <div className='calendar'>
+        <div className='calendar-month'>
+        </div>
+      </div>
     </div>
   )
 }
