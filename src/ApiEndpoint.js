cb14c7ddc7335f378fc09ca1c853dcc03516f21e
danfyfe <fyfedaniel@gmail.com>
2019-08-05 17:10:24 -0400

    user signup

---
+++ src/ApiEndpoint.js
@@ -0,0 +1,9 @@
+const getApiEndpoint = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return 'https://lets-do-something-api.herokuapp.com/api/v1'
+  } else {
+    return 'http://localhost:3000/api/v1'
+  }
+}
+
+export default getApiEndpoint()
--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/SignUp.css';
-
+import API_ENDPOINT from '../ApiEndpoint.js'
 
 
 const SignupPage = props => {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+
+
   const sendToLoginPage = () => {
     props.history.push('/')
   }
+
+  const createUser = (e) => {
+    e.preventDefault()
+
+    fetch(`${API_ENDPOINT}/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        user: {
+          username, password, passwordConfirm
+        }
+      })
+    }).then(resp=>resp.json())
+      .then(data => {
+        console.log(data)
+      })
+  }
+
   return(
     <>
     <div className='main'>
@@ -20,13 +47,13 @@ const SignupPage = props => {
 
           <form className='signup-form'>
             <label>Username</label>
-            <input className='signup-input' type='text' placeholder='  Username'/>
+            <input className='signup-input' type='text' placeholder='  Username' onChange={e => setUsername(e.target.value.toLowerCase())}/>
             <label>Password</label>
-            <input className='signup-input' type='password' placeholder='  Password'/>
+            <input className='signup-input' type='password' placeholder='  Password' onChange={e => setPassword(e.target.value)}/>
             <label>Confirm Password</label>
-            <input className='signup-input' type='password' placeholder='  Confirm Password'/>
+            <input className='signup-input' type='password' placeholder='  Confirm Password' onChange={e => setPasswordConfirm(e.target.value)}/>
             <div className='signup-button-container'>
-              <button>Submit</button>
+              <button onClick={createUser}>Submit</button>
               <button onClick={sendToLoginPage}>Cancel</button>
             </div>
           </form>
