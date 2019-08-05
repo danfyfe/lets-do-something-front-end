ffd7e979c5e38150e179bac70d97ef798e5d8127
danfyfe <fyfedaniel@gmail.com>
2019-08-05 15:47:17 -0400

    login form

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -12,11 +12,22 @@ const LoginPage = () => {
           <h1>Lets Do Something!</h1>
         </div>
         <div className='login-form-container'>
+
+          <h3>LogIn!</h3>
+
           <form className='login-form'>
             <input className='login-input' type='text' placeholder='  Username'/>
             <input className='login-input' type='password' placeholder='  Password'/>
+            <button>Submit</button>
           </form>
+
           <hr width={'75%'}/>
+
+          <div className='signup-container'>
+            <h4>or</h4>
+            <button>SignUp!</button>
+          </div>
+          
         </div>
       </div>
     </div>
