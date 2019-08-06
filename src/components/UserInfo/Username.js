8878a45a89b948849272802e637952edd291618c
danfyfe <fyfedaniel@gmail.com>
2019-08-06 16:57:25 -0400

    edit username

--- src/components/UserInfo/Username.js
+++ src/components/UserInfo/Username.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react'
 
 import { connect } from 'react-redux'
 
+import API_ENDPOINT from '../../ApiEndpoint.js'
+
 
 const Username = props => {
 
   const { username } = props
 
   const [ editing, setEditing ] = useState(false)
-  const [ newUsername, updateUsername ] = useState('')
+  const [ newUsername, setNewUsername ] = useState('')
 
   const renderEditButton = () => {
     return <>
@@ -19,13 +21,37 @@ const Username = props => {
   const renderEditForm = () => {
     return <>
       <form>
-        <input type='text' placeholder='New Username'/>
-        <button>Submit</button>
+        <input type='text' placeholder='New Username' onChange={(e) => setNewUsername(e.target.value)}/>
+        <button onClick={updateUsername}>Submit</button>
         <button onClick={() => setEditing(!editing)}>Cancel</button>
       </form>
     </>
   }
-  
+
+  const updateUsername = (e) => {
+    e.preventDefault()
+
+    fetch(`${API_ENDPOINT}/users/username`, {
+      method: 'PATCH',
+      headers: {
+        Authorization:  localStorage.getItem("token"),
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        oldUsername: username, username: { username: newUsername }
+      })
+    }).then(resp=>resp.json())
+    .then( data => {
+      if (data.error) {
+        props.setErrorMessage(data.error)
+      } else {
+        props.setCurrentUser(data.user)
+        setEditing(!editing)
+      }
+    })
+  }
+
   return(
     <>
       <label>Username: </label> <ul><li><span>{username}</span>{editing ? renderEditForm() : renderEditButton()}</li></ul>
@@ -39,7 +65,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-
+    setCurrentUser: user => dispatch({type:'SET_CURRENT_USER', user}),
+    setErrorMessage: errorMessage => dispatch({type:'SET_ERROR_MESSAGE', errorMessage})
   }
 }
 
--- src/containers/UserInfoContainer.js
+++ src/containers/UserInfoContainer.js
@@ -7,13 +7,15 @@ import Username from '../components/UserInfo/Username.js'
 
 class UserInfoContainer extends React.Component {
   render(){
-    const { username, email, firstName, lastName, avatar } = this.props.user
+    console.log(this.props.user)
+    const { username, email, firstName, lastName, dob, avatar } = this.props.user
     return (<>
       <div className='user-info-container'>
         <Username username={username}/>
         <label>First Name: </label> <ul><li><span>{firstName}</span><button>Edit</button></li></ul>
         <label>Last Name: </label> <ul><li><span>{lastName}</span><button>Edit</button></li></ul>
         <label>Email: </label> <ul><li><span>{email}</span><button>Edit</button></li></ul>
+        <label>Date of Birth: </label> <ul><li><span>{dob}</span><button>Edit</button></li></ul>
         <label>Avatar: </label> <ul><li><span><img src={avatar} alt='user avatar'/></span><button>Edit</button></li></ul>
       </div>
       </>)
