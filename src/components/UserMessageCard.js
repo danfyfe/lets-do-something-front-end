1f577b400ff1538a0321de4d49624709b0157192
danfyfe <fyfedaniel@gmail.com>
2019-08-13 18:48:50 -0400

    message card redo

--- src/components/UserMessageCard.js
+++ src/components/UserMessageCard.js
@@ -5,19 +5,31 @@ const UserMessageCard = props => {
     <div className='user-message-card'>
 
       <div className='user-message-card-top'>
-        <h5>Title</h5>
-        <div className='user-image-container'>
-          <img src='https://i.imgur.com/KmHDi3Q.jpg' alt='user'/>
+
+        <div className='user-message-card-top-left'>
+          <div className='user-message-card-title'>
+            <h5>Title</h5>
+          </div>
+          <div className='user-message-card-content'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nullam non nisi est sit amet facilisis.</p>
+          </div>
+        </div>
+
+        <div classname='user-message-card-top-right'>
+          <div className='user-image-container'>
+             <img src='https://i.imgur.com/KmHDi3Q.jpg' alt='user'/>
+          </div>
         </div>
       </div>
 
-      <div className='user-message-card-middle'>
-        <p>Content</p>
-      </div>
-
       <div className='user-message-card-bottom'>
+        <div className='user-message-card-bottom-left'>
+          <p>Event Name</p><p>Message Date</p>
+        </div>
+        <div className='user-message-card-bottom-right'>
+          <button>Reply</button>
+        </div>
       </div>
-
     </div>
   )
 }
@@ -25,3 +37,21 @@ const UserMessageCard = props => {
 export default UserMessageCard
 
 // <img src='https://i.imgur.com/KmHDi3Q.jpg' alt='user'/>
+
+
+      // <div className='user-message-card-top'>
+      //   <h5>Title</h5>
+      //   <div className='user-image-container'>
+      //     <img src='https://i.imgur.com/KmHDi3Q.jpg' alt='user'/>
+      //   </div>
+      // </div>
+      //
+      // <div className='user-message-card-middle'>
+      //
+      //     <p>Content</p>
+      //
+      // </div>
+      //
+      // <div className='user-message-card-bottom'>
+      //
+      // </div>
