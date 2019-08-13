6e3ad4ad5be39348f09216df33819a354cd1dade
danfyfe <fyfedaniel@gmail.com>
2019-08-13 12:09:24 -0400

    user messages container top

--- src/containers/UserMessagesContainer.js
+++ src/containers/UserMessagesContainer.js
@@ -5,10 +5,20 @@ import UserMessageCard from '../components/UserMessageCard.js'
 const UserMessagesContainer = props => {
   return(
     <div className='user-messages-container'>
+
       <div className='user-messages-top'>
-        <label>Messages</label>
-        <UserMessageCard message={props.message}/>
+
+        <div className='user-messages-top-left'>
+          <h4>Messages</h4>
+        </div>
+
+        <div className='user-messages-top-right'>
+          <h4>Sort By</h4>
+          <h4>+</h4>
+        </div>
       </div>
+
+      <UserMessageCard />
     </div>
   )
 
