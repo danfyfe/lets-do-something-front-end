f28e6439ba2f91a8543f7236610bfeb68c39baa6
danfyfe <fyfedaniel@gmail.com>
2019-08-15 17:01:54 -0400

    add hover

--- src/containers/EventsContainer.js
+++ src/containers/EventsContainer.js
@@ -5,12 +5,24 @@ import EventCard from '../components/EventCard.js'
 import '../css/Events.css';
 
 class EventsContainer extends React.Component {
+  state = {
+    creatingEvent: false
+  }
+
+  setCreatingEvent = () => {
+    this.setState({
+      creatingEvent: !this.state.creatingEvent
+    })
+  }
+
   render(){
+    console.log(this.state)
+
     return(
       <div className='events-container'>
         <div className='events-container-top'>
-          <h1 className='events-header'>Events</h1>
-          <h1 className='events-header'>+</h1>
+          <h1 className='events-container-header'>Events</h1>
+          <h1 className='events-container-add' onClick={this.setCreatingEvent}>+</h1>
         </div>
         <div className='event-cards-container'>
           <EventCard/>
