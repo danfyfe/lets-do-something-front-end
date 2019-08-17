7552061d6f9937947de1b0577fc79aa6d3199e90
danfyfe <fyfedaniel@gmail.com>
2019-08-17 09:47:15 -0400

    event card style

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -5,22 +5,23 @@ import AttendeeCard from './AttendeeCard.js'
 const EventCard = () => {
   return(
     <div className='event-card'>
-      <h3 className='event-title'>Event Title</h3>
-      <div className='event-time-container'>
-        <div className='event-time'>08/07/19 at 7pm</div>
-        <div className='event-time'>to</div>
-        <div className='event-time'>08/11/19 at 5pm</div>
+      <div className='event-card top'>
+        <div className='event-title-container'>
+          <h5 className='event-title'>Event Title</h5>
+        </div>
+
+        <div className='event-time-container'>
+          <p className='event-time'>08/07/19 at 7pm</p>
+          <p className='event-time'>to</p>
+          <p className='event-time'>08/11/19 at 5pm</p>
+        </div>
       </div>
+
       <div className='event-description'>
-        Event description! This is where you put the breif description of all the fun you will be having!
+        <p>Event description! This is where you put the breif description of all the fun you will be having!</p>
       </div>
       <div className='attendees-container'>
-        <AttendeeCard/>
-        <AttendeeCard/>
-        <AttendeeCard/>
-        <AttendeeCard/>
-        <AttendeeCard/>
-        <AttendeeCard/>
+        <p># Attendees</p>
       </div>
     </div>
   )
