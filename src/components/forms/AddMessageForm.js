e155dd890c6120d497b71bc89669876d0c57e952
danfyfe <fyfedaniel@gmail.com>
2019-08-21 14:30:02 -0400

    add message form cont2

--- src/components/forms/AddMessageForm.js
+++ src/components/forms/AddMessageForm.js
@@ -3,15 +3,26 @@ import React, { useState } from 'react'
 
 
 const AddMessageForm = props => {
+  const [ title, setTitle ] = useState('');
+  const [ content, setContent ] = useState('')
+
   return(
     <div className='add-message-form-container'>
       <div className='add-message-form'>
 
         <label>Title</label>
-        <input type='text' placeholder='Title'/>
+        <input
+          type='text'
+          placeholder='Title'
+          onChange={e => setTitle(e.target.value)}/>
 
         <label>Content</label>
-        <textarea type='text' rows='4' cols='10'placeholder='Content'/>
+        <textarea
+          type='text'
+          rows='4'
+          cols='10'
+          placeholder='Content'
+          onChange={e => setContent(e.target.value)}/>
 
 
       </div>
