4db6387fb83a16d8377e01e75ea505a56542dbae
danfyfe <fyfedaniel@gmail.com>
2019-08-21 13:00:15 -0400

    event card open

--- src/components/cards/EventCard.js
+++ src/components/cards/EventCard.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import moment from 'moment'
+import { Icon } from 'semantic-ui-react'
+
 
 // import AttendeeCard from './AttendeeCard.js'
 
 const EventCard = props => {
 
-  console.log(props.event)
+  const [ open, setOpen ] = useState(false)
+
   const { title, description, start, end, owner_id, users} = props.event
 
   const owner = users.find(user => user.id === owner_id)
 
   const formatTime = time => {
     return moment(time).format('MMM Do YYYY, h:mm a')
+  }
+
+  const renderOpenCloseIcon = () => {
+    if (open) {
+      return   <Icon link onClick={()=>setOpen(false)} size='normal' name='angle double up'/>
+    } else {
+      return   <Icon link onClick={()=>setOpen(true)} size='normal' name='angle double down'/>
+    }
   }
 
   return(
@@ -28,22 +39,31 @@ const EventCard = props => {
           <span className='event-time'>{formatTime(end)}</span>
         </div>
       </div>
-
-      <div className='event-description'>
+      <>{open ?
+        <>
+        <div className='event-description'>
         <p>{description}</p>
-      </div>
+        </div>
 
-      <div className='event-card-bottom'>
+        <div className='event-card-bottom'>
 
         <div className='attendees-container'>
-          <span>{users.length} Attendees</span>
+        <span>{users.length} Attendees</span>
         </div>
 
         <div className='owner-container'>
-          <span>Created By: {owner.username}</span>
+        <span>Created By: {owner.username}</span>
         </div>
 
-      </div>
+        </div>
+        </> : null
+      }</>
+
+        <div className='collapse-button-container'>
+          {renderOpenCloseIcon()}
+        </div>
+
+
     </div>
   )
 }
