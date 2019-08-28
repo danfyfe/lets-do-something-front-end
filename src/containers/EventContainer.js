76b89ff118cc29ae67503aa65a187fa124468e80
danfyfe <fyfedaniel@gmail.com>
2019-08-28 17:14:01 -0400

    event page

--- src/containers/EventContainer.js
+++ src/containers/EventContainer.js
@@ -1,14 +1,26 @@
 import React from 'react'
 
+import { formatTime } from '../actions/general.js'
+
 const EventContainer = props => {
   const { title, description, start, end, users } = props.event
 
+  
   return(
     <div className='d-flex flex-column most-width m-auto'>
-      <h4>{title}</h4>
-      
-      <span>{start}</span>
-      <span>{end}</span>
+      <div className='d-flex flex-row'>
+        <h4>{title}</h4>
+        <button>Edit</button>
+      </div>
+
+      <div className='d-flex flex-row'>
+        <span className='text-muted m-auto'>{formatTime(start)}</span>
+        <span className='text-muted m-auto'>{formatTime(end)}</span>
+      </div>
+
+      <div className='d-flex med-padding'>
+        <p className='m-auto'>{description}</p>
+      </div>
     </div>
   )
 }
