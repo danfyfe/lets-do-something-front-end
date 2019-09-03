7d11d1f6818cd990affe5a1f771f5e93ce648214
danfyfe <fyfedaniel@gmail.com>
2019-09-03 16:49:38 -0400

    create follow request

--- src/components/cards/UserCard.js
+++ src/components/cards/UserCard.js
@@ -7,12 +7,14 @@ import API_ENDPOINT from '../../ApiEndpoint.js'
 const UserCard = props => {
   // console.log(props.user)
 
-  const { username, image, first_name, last_name } = props.user
+  const { username, image, first_name, last_name, following_users, followered_users } = props.user
 
   const createFriendRequest = username => {
-    fetch(`${API_ENDPOINT}/friendrequests/${props.currentUser.id}`, {
+    fetch(`${API_ENDPOINT}/followrequest/${props.currentUser.id}`, {
       method:'POST',
       headers: {
+        "Content-Type": "application/json",
+        Accept: 'application/json',
         Authorization:  localStorage.getItem("token")
       },
       body: JSON.stringify({
@@ -23,7 +25,7 @@ const UserCard = props => {
       console.log(result)
     })
   }
-  
+  console.log(props.currentUser)
   return(
     <div className='user-card d-flex flex-row justify-content-between yellow-background med-padding'>
 
@@ -35,7 +37,7 @@ const UserCard = props => {
       </div>
 
       <div className='d-flex flex-column small-padding half-width'>
-        <FontAwesomeIcon className='m-auto' icon='user-plus' onClick={()=>createFriendRequest(username)}/>
+        <FontAwesomeIcon className='m-auto link' icon='user-plus' onClick={()=>createFriendRequest(username)}/>
       </div>
 
     </div>
