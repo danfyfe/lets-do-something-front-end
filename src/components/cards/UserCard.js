e722edd11b8edae3c9c2d4620c622753f5500136
danfyfe <fyfedaniel@gmail.com>
2019-09-03 15:44:40 -0400

    start friend req

--- src/components/cards/UserCard.js
+++ src/components/cards/UserCard.js
@@ -1,19 +1,29 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const UserCard = props => {
-  console.log(props.user)
+  // console.log(props.user)
 
-  const { username , image, first_name, last_name } = props.user
+  const { username, image, first_name, last_name } = props.user
+
+  const requestFriend = username => {
+    
+  }
 
   return(
-    <div className='d-flex flex-column yellow-background med-padding'>
-      <div className='d-flex justify-content-between med-padding'>
-        <img src={image} alt='user avatar'/>
-        <span className='text-nowrap '>{username}</span>
+    <div className='user-card d-flex flex-row justify-content-between yellow-background med-padding'>
+
+      <div className='d-flex flex-column justify-content-between med-padding'>
+        {/*<img className='user-image' src='' alt='user avatar'/>*/}
+
+        <span className='text-nowrap font-weight-bold med-font med-padding m-auto'>{username}</span>
+        {/*<span className='text-muted small-font'>{first_name + ' ' + last_name}</span>*/}
       </div>
-      <div className='dflex'>
-        <span className='text-muted small-font'>{first_name + ' ' + last_name}</span>
+
+      <div className='d-flex flex-column small-padding half-width'>
+        <FontAwesomeIcon className='m-auto' icon='user-plus' onClick={()=>requestFriend(username)}/>
       </div>
+
     </div>
   )
 }
