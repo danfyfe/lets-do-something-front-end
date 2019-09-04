6b95ac7ffdd3b7aa36431ca1d3c92366e5ef2014
danfyfe <fyfedaniel@gmail.com>
2019-09-04 17:10:57 -0400

    invite form redo

--- src/components/forms/AddInviteForm.js
+++ src/components/forms/AddInviteForm.js
@@ -1,46 +1,91 @@
 import React, { useState } from 'react'
-
-import InviteSelect from './InviteSelect.js'
+import { connect } from 'react-redux'
+// import InviteSelect from './InviteSelect.js'
 
 const AddInviteForm = props => {
+  const [ invitees, setInvitees ] = useState([])
 
-  const { eventId, inputs, removeSpecificSelect, values, setValues } = props
+  const { followers } = props
 
-  const addSelectValue = (value, id) => {
-    // setValues([...values, value])
-    setValues([...values, {id, value}])
-    console.log('VALUE', value)
-    console.log('ID', id)
+
+  const handleChange = e => {
+    if (e.target.checked) {
+      setInvitees([...invitees, e.target.value])
+    } else {
+      let index = invitees.indexOf(e.target.value)
+      invitees.splice(index, 1)
+    }
   }
 
-  const removeSelectValue = id => {
-    let selectValue = values.find( value => value.id === id)
-    // console.log('select value', selectValue)
-    let index = values.indexOf(selectValue)
+  const followerInputs = () => {
+    return followers.map( follower => {
+      return <div key={follower.id} className='d-flex'>
 
-    values.splice(index, 1)
+          <div className='col-sm'>
+            <label className='small-font'>{follower.username}</label>
+          </div>
 
-    console.log('VALUES',values)
-  }
+          <div className='col-sm'>
+            <input className='' type='checkbox' value={follower.username} onChange={handleChange}/>
+          </div>
 
-  const renderSelects = () => {
-    return inputs.map( input => {
-      return <InviteSelect
-      id={input}
-      key={input}
-      addSelectValue={addSelectValue}
-      removeSelectValue={removeSelectValue}
-      removeSpecificSelect={removeSpecificSelect}/>
+      </div>
     })
   }
 
 
+  console.log('invitees',invitees)
+
   return(
-    <div className='d-flex flex-column m-1v'>
-        {renderSelects()}
-        <button className='small-font'>Send Invites!</button>
+    <div className='d-flex flex-column m-1v med-padding'>
+
+      <span className='small-font m-auto'>Select Friends to Invite!</span>
+
+      <div className='d-flex flex-column b-thin med-padding'>
+        {followerInputs()}
+      </div>
+
+      <button className='small-font full-width' value='submit'>Send Invites!</button>
+
     </div>
   )
 }
 
-export default AddInviteForm
+const mapStateToProps = state => {
+  return {
+    followers: state.currentUser.followers
+  }
+}
+
+export default connect(mapStateToProps, null)(AddInviteForm)
+
+
+
+
+  // const { eventId, inputs, removeSpecificSelect, values, setValues } = props
+  //
+  // const addSelectValue = (value, id) => {
+  //   setValues([...values, {id, value}])
+  //   console.log('VALUE', value)
+  //   console.log('ID', id)
+  // }
+  //
+  // const removeSelectValue = id => {
+  //   let selectValue = values.find( value => value.id === id)
+  //   let index = values.indexOf(selectValue)
+  //
+  //   values.splice(index, 1)
+  //
+  //   console.log('VALUES',values)
+  // }
+  //
+  // const renderSelects = () => {
+  //   return inputs.map( input => {
+  //     return <InviteSelect
+  //     id={input}
+  //     key={input}
+  //     addSelectValue={addSelectValue}
+  //     removeSelectValue={removeSelectValue}
+  //     removeSpecificSelect={removeSpecificSelect}/>
+  //   })
+  // }
--- src/containers/InvitesContainer.js
+++ src/containers/InvitesContainer.js
@@ -4,57 +4,74 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import AddInviteForm from '../components/forms/AddInviteForm.js'
 
 const AddInvitesContainer = props => {
-  const { eventId } = props
-  const [ inputs, setInputs ] = useState([1])
-  const [ values, setValues ] = useState([])
+  // const { eventId } = props
+  // const [ inputs, setInputs ] = useState([1])
+  // const [ values, setValues ] = useState([])
+  //
+  // const addSelect = () => {
+  //   let newInput
+  //   if (inputs.length === 0) {
+  //     newInput = 1
+  //   } else {
+  //     newInput = inputs[inputs.length-1] + 1
+  //   }
+  //   setInputs([...inputs, newInput])
+  // }
+  //
+  // const removeLastSelect = () => {
+  //   inputs.splice(-1,1)
+  //   setInputs([...inputs])
+  //   values.splice(-1,1)
+  //   setValues([...values])
+  // }
+  //
+  // const removeSpecificSelect = id => {
+  //   let index = inputs.indexOf(id)
+  //   console.log('removeSpecificSelect', index)
+  // }
+  //
+  // console.log('values', values)
+  //
+  // return(<>
+  //   <div className='d-flex justify-content-between'>
+  //     <span className='font-weight-bold small-font'>Invites</span>
+  //     <div className='d-flex quart-width justify-content-around'>
+  //       <FontAwesomeIcon className='' icon='plus' onClick={addSelect}/>
+  //       <FontAwesomeIcon className='' icon='minus' onClick={removeLastSelect}/>
+  //     </div>
+  //   </div>
+  //
+  //   <div>
+  //     <AddInviteForm
+  //       eventId={eventId}
+  //       inputs={inputs}
+  //       values={values}
+  //       setValues={setValues}
+  //       removeSpecificSelect={removeSpecificSelect}/>
+  //   </div>
+  //
+  //   <div>
+  //     <span className='small-font text-muted'>Pending</span>
+  //
+  //   </div>
+  //
+  // </>)
 
-  const addSelect = () => {
-    let newInput
-    if (inputs.length === 0) {
-      newInput = 1
-    } else {
-      newInput = inputs[inputs.length-1] + 1
-    }
-    setInputs([...inputs, newInput])
-  }
-
-  const removeLastSelect = () => {
-    inputs.splice(-1,1)
-    setInputs([...inputs])
-    values.splice(-1,1)
-    setValues([...values])
-  }
-
-  const removeSpecificSelect = id => {
-    let index = inputs.indexOf(id)
-    console.log('removeSpecificSelect', index)
-  }
-
-  console.log('values', values)
+  const [ adding, setAdding ] = useState(true)
 
   return(<>
-    <div className='d-flex justify-content-between'>
-      <span className='font-weight-bold small-font'>Invites</span>
-      <div className='d-flex quart-width justify-content-around'>
-        <FontAwesomeIcon className='' icon='plus' onClick={addSelect}/>
-        <FontAwesomeIcon className='' icon='minus' onClick={removeLastSelect}/>
+    <div>
+      <div className='d-flex justify-content-between'>
+
+        <span className='font-weight-bold small-font'>Invites</span>
+        <div className='d-flex'>
+          <FontAwesomeIcon className='' icon='plus' onClick={()=>setAdding(true)}/>
+        </div>
       </div>
+
+      {adding ? <AddInviteForm/> : null}
+      
     </div>
-
-    <div>
-      <AddInviteForm
-        eventId={eventId}
-        inputs={inputs}
-        values={values}
-        setValues={setValues}
-        removeSpecificSelect={removeSpecificSelect}/>
-    </div>
-
-    <div>
-      <span className='small-font text-muted'>Pending</span>
-
-    </div>
-
   </>)
 }
 
