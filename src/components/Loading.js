2c13cfc16b4afef909c5d857ea6c2b3512cde873
danfyfe <fyfedaniel@gmail.com>
2019-09-05 17:29:10 -0400

    loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,16 +1,35 @@
-import React from 'react'
-
-// import '../css/loading.css'
-
+import React, { useState, useEffect } from 'react'
 
 const Loading = props => {
+  const [ array, setArray ] = useState([1])
+
+  const loadingArray = [{id:1, letter:'L'}, {id:2, letter:'O'},{id:3, letter:'A'},{id:4, letter:'D'},{id:5, letter:'I'},{id:6, letter:'N'},{id:7, letter:'G'},{id:8, letter:'.'},{id:9, letter:'.'},{id:10, letter:'.'}]
+
+
+
+
+  const span = letter => {
+    return <span id={letter.id} key={letter.id}>{letter.letter}</span>
+  }
+
+  const renderLoadingArray = () => {
+    return loadingArray.map(function(letter){
+      return span(letter)
+    })
+  }
+
+
   return(
     <div className='loading'>
-      LOADING...LOADING...LOADING
+
+      <div id='div'>
+        {renderLoadingArray()}
+      </div>
     </div>
   )
-
 
 }
 
 export default Loading
+
+// 'L','O','A','D','I','N','G','.','.','.','L','O','A','D','I','N','G','.','.','.','L','O','A','D','I','N','G','.','.','.'
